19d372451e769a1e1fd0bd51802276f268989a7c
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-11-26 19:50:30 +0300

    add binding

--- app/src/main/java/com/hellcorp/selfdictation/MainActivity.kt
+++ app/src/main/java/com/hellcorp/selfdictation/MainActivity.kt
@@ -2,10 +2,13 @@ package com.hellcorp.selfdictation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hellcorp.selfdictation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
